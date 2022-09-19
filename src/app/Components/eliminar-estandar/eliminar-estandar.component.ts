977ee1fa870b8549e7e47bf1d3507a25585521a1
Reyk Mrm <8428349+reykmrm@users.noreply.github.com>
2022-09-19 13:17:22 -0500

    se habilitaron las funciones de categoria, subcategoria y estandar

--- src/app/Components/eliminar-estandar/eliminar-estandar.component.ts
+++ src/app/Components/eliminar-estandar/eliminar-estandar.component.ts
@@ -1,23 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-eliminar-estandar',
-  templateUrl: './eliminar-estandar.component.html',
-  styleUrls: ['./eliminar-estandar.component.scss']
+  selector: "app-eliminar-estandar",
+  templateUrl: "./eliminar-estandar.component.html",
+  styleUrls: ["./eliminar-estandar.component.scss"],
 })
 export class EliminarEstandarComponent implements OnInit {
+  Estandar = {
+    IdEstandar: "",
+  };
+  constructor(private authService: AuthService, private router: Router) {}
 
-  Estandar={
-    IdEstandar:""
-  }
-  constructor(
-    private authService: AuthService,
-    private router: Router
-  ) { }
+
+  resultados = {}
 
   ngOnInit() {
+    this.getStandares()
+  }
+
+  getStandares() {
+    this.authService.getStandares(this.Estandar).subscribe((res: any) => {
+      this.resultados = res.map((item) => {
+        return item;
+      });
+    });
+  }
+
+  deleteEstandar(){
+    console.log("hola: ", this.Estandar)
+    this.authService.crear_categoria(this.Estandar).subscribe((res: any) => {
+      console.log(res);
+    });
   }
 
 }
