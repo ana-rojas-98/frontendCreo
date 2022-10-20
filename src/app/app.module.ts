7eb95e69ec676a382f8f91fe66f3b6665a4efe5b
Juan-Sandoval10 <juan.sandoval.ing@gmail.com>
2022-10-20 11:34:21 -0500

    Cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import { FooterPrincipalComponent } from './Components/footer-principal/footer-p
 import { ConfiguracionComponent } from './Components/configuracion/configuracion.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AsignarIndicadorComponent } from './Components/asignar-indicador/asignar-indicador.component';
+import { GoogleChartsModule } from 'angular-google-charts';
 //import { TokenInterceptorService } from './services/token-interceptor.service';
 
 @NgModule({
@@ -81,6 +82,7 @@ import { AsignarIndicadorComponent } from './Components/asignar-indicador/asigna
     HttpClientModule,
     NgbModule,
     FormsModule,
+    GoogleChartsModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },            
