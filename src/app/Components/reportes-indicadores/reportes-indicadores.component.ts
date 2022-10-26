75c318c20931dd1d5c30ae33f461fa6ecbf914a8
Reyk Mrm <8428349+reykmrm@users.noreply.github.com>
2022-10-26 10:50:19 -0500

    filtros habilitadoa en reporte inicadores

--- src/app/Components/reportes-indicadores/reportes-indicadores.component.ts
+++ src/app/Components/reportes-indicadores/reportes-indicadores.component.ts
@@ -16,6 +16,9 @@ export class ReportesIndicadoresComponent implements OnInit {
   Estandar = {
     estandar: "",
   };
+  Indicador = {
+    idIndicador: "",
+  };
 
   Usuario = {
     usuario: "",
@@ -36,28 +39,29 @@ export class ReportesIndicadoresComponent implements OnInit {
   resultadoEstandar: {};
   resultadosSubCategoria: {};
   resultadoIndicadores: [];
-  resultadosTabla= [];
+  resultadosTabla = [];
   estado = [];
 
   ngOnInit() {
-    this.GetUsuarios();
-    this.getCategoria();
-    this.getStandares();
-    this.getSubCategoria();
-    this.getindIcadores();
+    this.GetUsuarios(0);
+    this.getCategoria(0);
+    this.getStandares(0);
+    this.getSubCategoria(0);
+    this.getindIcadores(0);
   }
 
   getUsuarioFilter() {
     if (this.Usuario.usuario == "") {
-      this.getindIcadores();
       return true;
     }
+
+    let dato = parseInt(this.Usuario.usuario);
 
     this.reportesService
       .ConsultarIndicadoresAsignados()
       .subscribe((res: any) => {
         this.resultadosTabla = res.filter((item) => {
-          return item.idUsuario == parseInt(this.Usuario.usuario);
+          return item.idUsuario == dato;
         });
         this.estado = this.resultadosTabla;
       });
@@ -66,82 +70,121 @@ export class ReportesIndicadoresComponent implements OnInit {
   getEstandarFilter() {
     if (this.Estandar.estandar == "") {
       this.getUsuarioFilter();
+      this.getCategoria(0);
       return true;
     }
-
-    // this.resultadosTabla = this.estado.filter((item) => {
-    //   return item.estado == parseInt(this.estadoSelecionado.id);
-    // });
-
+    let dato = parseInt(this.Estandar.estandar);
+    this.getCategoria(dato);
+    this.resultadosTabla = this.estado.filter((item) => {
+      return item.idEstandar == dato;
+    });
   }
 
   getCategoriaFilter() {
     if (this.Categoria.categoria1 == "") {
       this.getEstandarFilter();
+      this.getSubCategoria(0);
       return true;
     }
-    this.getEstandarFilter();
-    if (
-      this.Usuario.usuario != "" &&
-      this.Estandar.estandar != "" &&
-      this.Categoria.categoria1 != ""
-    ) {
-      this.reportesService
-        .ConsultarIndicadoresAsignados()
-        .subscribe((res: any) => {
-          this.resultadosTabla = res.filter((item) => {
-            return (
-              item.idUsuario == parseInt(this.Usuario.usuario) &&
-              item.idEstandar == parseInt(this.Estandar.estandar) &&
-              item.idCategoria == parseInt(this.Categoria.categoria1)
-            );
-          });
-        });
+
+    let dato = parseInt(this.Categoria.categoria1);
+    this.getSubCategoria(dato);
+
+    this.resultadosTabla = this.estado.filter((item) => {
+      return item.idCategoria == dato;
+    });
+  }
+
+  geSubtCategoriaFilter() {
+    if (this.SubCategoria.subcategoria1 == "") {
+      this.getCategoriaFilter();
+      this.getindIcadores(0);
+      return true;
     }
 
-    
+    let dato = parseInt(this.SubCategoria.subcategoria1);
+    this.getindIcadores(dato);
 
+    this.resultadosTabla = this.estado.filter((item) => {
+      return item.idSubCategoria == dato;
+    });
   }
+  getIndicadorFilter() {
+    if (this.Indicador.idIndicador == "") {
+      this.geSubtCategoriaFilter();
+      return true;
+    }
 
-  GetUsuarios() {
-    this.reportesService.GetUsuarios().subscribe((res: any) => {
-      this.resultadosUsuario = res.map((item) => {
-        return item;
-      });
+    this.resultadosTabla = this.estado.filter((item) => {
+      return item.idIndicador == parseInt(this.Indicador.idIndicador);
     });
   }
 
-  getStandares() {
-    this.authService.getStandares("").subscribe((res: any) => {
-      this.resultadoEstandar = res.map((item) => {
-        return item;
-      });
-    });
-  }
-
-  getCategoria() {
-    this.authService.getCategoria("").subscribe((res: any) => {
-      this.resultadosCategoria = res.map((item) => {
-        return item;
-      });
-    });
-  }
-
-  getSubCategoria() {
-    this.authService.getSubCategoria("").subscribe((res: any) => {
-      this.resultadosSubCategoria = res.map((item) => {
-        return item;
-      });
-    });
-  }
-  getindIcadores() {
-    this.reportesService
-      .ConsultarIndicadoresAsignados()
-      .subscribe((res: any) => {
-        this.resultadosTabla = res.map((item) => {
-          this.resultadoIndicadores = res;
+  GetUsuarios(dato) {
+    if (dato == 0) {
+      this.reportesService.GetUsuarios().subscribe((res: any) => {
+        this.resultadosUsuario = res.map((item) => {
           return item;
         });
       });
+    }
+  }
+
+  getStandares(dato) {
+    if (dato == 0) {
+      this.authService.getStandares("").subscribe((res: any) => {
+        this.resultadoEstandar = res.map((item) => {
+          return item;
+        });
+      });
+      return true;
+    }
+  }
+
+  getCategoria(dato) {
+    if (dato == 0) {
+      this.authService.getCategoria("").subscribe((res: any) => {
+        this.resultadosCategoria = res.map((item) => {
+          return item;
+        });
+      });
+      return true;
+    }
+    this.authService.getCategoria("").subscribe((res: any) => {
+      this.resultadosCategoria = res.filter((item) => {
+        return item.idEstandar == dato;
+      });
+    });
+  }
+
+  getSubCategoria(dato) {
+    if (dato == 0) {
+      this.authService.getSubCategoria("").subscribe((res: any) => {
+        this.resultadosSubCategoria = res.map((item) => {
+          return item;
+        });
+      });
+      return true;
+    }
+
+    this.authService.getSubCategoria("").subscribe((res: any) => {
+      this.resultadosSubCategoria = res.filter((item) => {
+        return item.idCategoria == dato;
+      });
+    });
+  }
+  getindIcadores(dato) {
+    if (dato == 0) {
+      this.reportesService
+        .ConsultarIndicadoresAsignados()
+        .subscribe((res: any) => {
+          this.resultadosTabla = res.map((item) => {
+            this.estado = res;
+            this.resultadoIndicadores = res;
+            return item;
+          });
+        });
+      return true;
+    }
   }
 }
