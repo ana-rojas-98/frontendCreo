234167f89e6b058f3ee8f3ad6e3bb933bf302c6e
Reyk Mrm <8428349+reykmrm@users.noreply.github.com>
2022-11-14 21:48:13 -0500

    merge

--- src/app/Components/editar-indicador/editar-indicador.component.ts
+++ src/app/Components/editar-indicador/editar-indicador.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { AuthService } from "src/app/services/auth.service";
 import { FormControl } from "@angular/forms";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 @Component({
-  selector: 'app-editar-indicador',
-  templateUrl: './editar-indicador.component.html',
-  styleUrls: ['./editar-indicador.component.scss']
+  selector: "app-editar-indicador",
+  templateUrl: "./editar-indicador.component.html",
+  styleUrls: ["./editar-indicador.component.scss"],
 })
 export class EditarIndicadorComponent implements OnInit {
-
-  constructor(private authService: AuthService, private route: ActivatedRoute, public router: Router) { }
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    public router: Router
+  ) {}
   Estandar = new FormControl("");
   Categoria = new FormControl("");
   Subcategoria = new FormControl("");
@@ -20,22 +23,21 @@ export class EditarIndicadorComponent implements OnInit {
   accionEditar = "";
   editar = false;
   resultadosTabla = [];
-  resultado:any=[];
+  resultado: any = [];
   resultadosHTML = [];
   Respuestas = [];
   prueba = "";
   idArchivo = {
     idArchivo: 1,
   };
-  resultados=[];
+  resultados = [];
   resultadosCategoria = {};
   resultadosSubCategoria = {};
-  datos={
-    Estandar:"",
-    Categoria:"",
-    Subcategoria:"",
-
-  }
+  datos = {
+    Estandar: "",
+    Categoria: "",
+    Subcategoria: "",
+  };
 
   ngOnInit() {
     this.id = parseInt(this.route.snapshot.paramMap.get("id"));
@@ -47,18 +49,19 @@ export class EditarIndicadorComponent implements OnInit {
     this.TraerFormato();
     this.getStandares();
   }
-  TraerFormato(){
-    this.authService.EditarIndicador().subscribe((respuesta: any)=>{
-      this.resultado = respuesta.map((item)=>{
-        if(this.idArchivo.idArchivo == item.idArchivo){
-          this.resultadosTabla.push(item)
+  TraerFormato() {
+    console.log("Trae formato");
+    this.authService.EditarIndicador().subscribe((respuesta: any) => {
+      this.resultado = respuesta.map((item) => {
+        if (this.idArchivo.idArchivo == item.idArchivo) {
+          this.resultadosTabla.push(item);
           this.datos.Estandar = item.nombreEstandar;
           this.datos.Categoria = item.nombreCategoria;
           this.datos.Subcategoria = item.nombreSubcategoria;
           console.log("formato", this.resultadosTabla);
         }
         return item;
-      });           
+      });
     });
   }
   getStandares() {
@@ -78,47 +81,45 @@ export class EditarIndicadorComponent implements OnInit {
   }
 
   getSubCategoria(categoria) {
-      this.authService
-        .getSubCategoria(this.datos.Categoria)
-        .subscribe((res: any) => {
-          this.resultadosSubCategoria = res.filter(
-            (item) => item.nombreCategoria == categoria
-          );
-        });
+    this.authService
+      .getSubCategoria(this.datos.Categoria)
+      .subscribe((res: any) => {
+        this.resultadosSubCategoria = res.filter(
+          (item) => item.nombreCategoria == categoria
+        );
+      });
   }
-enviar:any = [];
-  guardar(){
-    console.log("resulatado",this.resultadosTabla)
-    console.log("resulatado",this.datos.Estandar)
-    this.resultadosTabla.map((item)=>{
-        this.enviar.push(
-          {
-            entrada: item.entrada,
-            numerop: item.numerop,
-            formulap: item.formulap,
-            formula: item.formula,
-            valor: item.valor,
-            titulo: item.titulo,
-            tamanoTexto: item.tamanoTexto,
-            color: item.color,
-            negrilla: item.negrilla,
-            subrayado: item.subrayado,
-            cursiva: item.cursiva,
-            inicioCol: item.inicioCol,
-            finCol: item.finCol,
-            saltoLinea: item.saltoLinea,
-            html:item.html,
-            idArchivo: item.idArchivo,
-            nombreEstandar :this.datos.Estandar,
-            nombreCategoria: this.datos.Categoria,
-            nombreSubcategoria: this.datos.Subcategoria,
-          }
-        ); 
+  enviar: any = [];
+  guardar() {
+    console.log("resulatado", this.resultadosTabla);
+    console.log("resulatado", this.datos.Estandar);
+    this.resultadosTabla.map((item) => {
+      this.enviar.push({
+        entrada: item.entrada,
+        numerop: item.numerop,
+        formulap: item.formulap,
+        formula: item.formula,
+        valor: item.valor,
+        titulo: item.titulo,
+        tamanoTexto: item.tamanoTexto,
+        color: item.color,
+        negrilla: item.negrilla,
+        subrayado: item.subrayado,
+        cursiva: item.cursiva,
+        inicioCol: item.inicioCol,
+        finCol: item.finCol,
+        saltoLinea: item.saltoLinea,
+        html: item.html,
+        idArchivo: item.idArchivo,
+        nombreEstandar: this.datos.Estandar,
+        nombreCategoria: this.datos.Categoria,
+        nombreSubcategoria: this.datos.Subcategoria,
+      });
     });
-    console.log("enviar",this.enviar)
-    this.authService.enviarIndicadorEsitado(this.enviar).subscribe((res)=>{
-      if(res){
-        this.alerta("Editado")
+    console.log("enviar", this.enviar);
+    this.authService.enviarIndicadorEsitado(this.enviar).subscribe((res) => {
+      if (res) {
+        this.alerta("Editado");
       }
       return res;
     });
