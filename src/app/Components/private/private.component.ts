2ba0124b0dee34509c9bdbe189d958c5a5c1a3a3
Emmanuel Renteria <elpantera142@hotmail.com>
2022-11-24 11:21:40 -0500

    Refactory private  filtro indicadores asignados

--- src/app/Components/private/private.component.ts
+++ src/app/Components/private/private.component.ts
@@ -8,64 +8,89 @@ import { AuthService } from "src/app/services/auth.service";
   styleUrls: ["./private.component.scss"],
 })
 export class PrivateComponent implements OnInit {
-  constructor(private authService: AuthService,
-    private reportesService: ReportesService) {}
+  constructor(
+    private authService: AuthService,
+    private reportesService: ReportesService
+  ) {}
   usarioLocalStote = JSON.parse(localStorage.getItem("usario"));
-  mostrar=false;
-  resultadosTabla:any=[];
-  resultados:any=[];
-  indicadores:any=[];
-  avance:any=[];
+  mostrar = false;
+  resultadosTabla: any = [];
+  resultados: any = [];
+  indicadores: any = [];
+  avance: any = [];
 
   ngOnInit() {
     this.authService.enviarCorreos().subscribe((res: any) => {});
     this.authService.enviarCorreosIndicadores().subscribe((res: any) => {});
-    if(this.usarioLocalStote.typeuser=='1'){
-        this.mostrar=true;
-    }else if(this.usarioLocalStote.typeuser=='2'){
-        this.mostrar=true;
-    }else if(this.usarioLocalStote.typeuser=='3'){
-        this.mostrar=false;
+    if (this.usarioLocalStote.typeuser == "1") {
+      this.mostrar = true;
+    } else if (this.usarioLocalStote.typeuser == "2") {
+      this.mostrar = true;
+    } else if (this.usarioLocalStote.typeuser == "3") {
+      this.mostrar = false;
+    }
+    this.pantallaPrin();
   }
-  this.pantallaPrin();
-  }
-  
-  activos=0;
-  inactivos=0;
-  total=0;
-  completados=0;
-  faltantes=0;
-  totales=0;
-  pantallaPrin(){
+
+  activos = 0;
+  inactivos = 0;
+  total = 0;
+  completados = 0;
+  faltantes = 0;
+  totales = 0;
+  pantallaPrin() {
     this.authService.getUsuarios("").subscribe((res: any) => {
-      this.resultados=res.map((item)=>{
+      this.resultados = res.map((item) => {
         this.resultadosTabla.push(item);
-        if(item.estado=='1'){
+        if (item.estado == "1") {
           this.activos++;
-        }else if(item.estado=='0'){
+        } else if (item.estado == "0") {
           this.inactivos++;
         }
-      })
-      console.log("inactivos",this.inactivos)
-      console.log("activos",this.activos)
-      this.total=this.activos+this.inactivos;
-      console.log("total",this.total)
       });
-      console.log("array que trae", this.resultadosTabla)
+      console.log("inactivos", this.inactivos);
+      console.log("activos", this.activos);
+      this.total = this.activos + this.inactivos;
+      console.log("total", this.total);
+    });
+    console.log("array que trae", this.resultadosTabla);
+    if (this.usarioLocalStote.typeuser == "3") {
+      let id = {
+        id: this.usarioLocalStote.usuarioid,
+      };
       this.reportesService
-      .ConsultarIndicadoresAsignados()
-      .subscribe((res: any) => {
-        this.indicadores= res.map((item) => {
-          if(item.avance==100){
-            this.completados++;
-          }else if(item.avance>=0 && item.avance<100){
-            this.faltantes++;
-          }
+        .IndicadoresAsignados(id)
+        .subscribe((res: any) => {
+          this.indicadores = res.map((item) => {
+            if (item.avance == 100) {
+              this.completados++;
+            } else if (item.avance >= 0 && item.avance < 100) {
+              this.faltantes++;
+            }
+          });
+          console.log("completados", this.completados);
+          console.log("faltantes", this.faltantes);
+          this.totales = this.completados + this.faltantes;
+          console.log("totales", this.totales);
         });
-        console.log("completados",this.completados)
-        console.log("faltantes",this.faltantes)
-        this.totales=this.completados+this.faltantes;
-        console.log("totales",this.totales)
-      });
+    }
+
+    if (this.usarioLocalStote.typeuser != "3") {
+      this.reportesService
+        .ConsultarIndicadoresAsignados()
+        .subscribe((res: any) => {
+          this.indicadores = res.map((item) => {
+            if (item.avance == 100) {
+              this.completados++;
+            } else if (item.avance >= 0 && item.avance < 100) {
+              this.faltantes++;
+            }
+          });
+          console.log("completados", this.completados);
+          console.log("faltantes", this.faltantes);
+          this.totales = this.completados + this.faltantes;
+          console.log("totales", this.totales);
+        });
+    }
   }
 }
